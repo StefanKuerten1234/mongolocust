ecdc4e5df74bd895e6fdf45497c94a6fd3ffae00
Stefan Kürten <stefan@kuerten.family>
2024-07-14 13:24:29 +0200

    WIP: Mock Presentation prep

--- settings.py
+++ settings.py
@@ -1,13 +1,13 @@
 import os
 
-DEFAULTS = {'DB_NAME': 'sample',
-            'COLLECTION_NAME': 'documents',
-            'CLUSTER_URL': f'mongodb+srv://user:password@something.mongodb.net/sample?retryWrites=true&w=majority',
-            'DOCS_PER_BATCH': 100,
-            'INSERT_WEIGHT': 1,
-            'FIND_WEIGHT': 3,
+DEFAULTS = {'DB_NAME': 'loadtest',
+            'COLLECTION_NAME': 'measurements',
+            'CLUSTER_URL': f'mongodb+srv://stefan:STAVfvCkuHN0FgyT@stefans-playground.ex1vt.mongodb.net/',
+            'DOCS_PER_BATCH': 1000,
+            'INSERT_WEIGHT': 0,
+            'FIND_WEIGHT': 0,
             'BULK_INSERT_WEIGHT': 1,
-            'AGG_PIPE_WEIGHT': 1}
+            'AGG_PIPE_WEIGHT': 0}
 
 
 def init_defaults_from_env():
