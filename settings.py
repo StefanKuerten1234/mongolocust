e46ab2806869de4a5d767fb0a211a7c6a300fc2f
Stefan Kürten <stefan@kuerten.family>
2024-07-18 08:55:47 +0200

    Reduce batch size for Timeseries insert

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import os
 DEFAULTS = {'DB_NAME': 'loadtest',
             'COLLECTION_NAME': 'measurements_ts',
             'CLUSTER_URL': f'mongodb+srv://stefan:STAVfvCkuHN0FgyT@atlascluster.am7mj2q.mongodb.net/',
-            'DOCS_PER_BATCH': 150,
+            'DOCS_PER_BATCH': 50,
             'INSERT_WEIGHT': 0,
             'FIND_WEIGHT': 0,
             'BULK_INSERT_WEIGHT': 100,
