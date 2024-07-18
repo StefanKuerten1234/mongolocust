6db18fa31eec6c2c6db273d68e725245e1f7fc8e
Stefan Kürten <stefan@kuerten.family>
2024-07-18 16:20:23 +0200

    WIP

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ import os
 
 DEFAULTS = {'DB_NAME': 'loadtest',
             'COLLECTION_NAME': 'measurements_ts',
-            'CLUSTER_URL': f'mongodb+srv://stefan:STAVfvCkuHN0FgyT@atlascluster.am7mj2q.mongodb.net/',
+            'CLUSTER_URL': f'mongodb+srv://stefan:xxx@atlascluster.am7mj2q.mongodb.net/',
             'DOCS_PER_BATCH': 50,
             'INSERT_WEIGHT': 0,
             'FIND_WEIGHT': 0,
